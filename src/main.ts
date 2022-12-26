70730e6b89cb452c8904a03b539ee1e247899243
snape1112 <107560795+snape1112@users.noreply.github.com>
2022-12-26 11:10:10 +0200

    monitor events of cryptopunk contract

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,35 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
 import { AppModule } from './app.module';
+import { ethers } from 'ethers';
+import fs from 'fs';
 import type {
   CorsConfig,
   NestConfig,
   SwaggerConfig,
 } from 'src/common/configs/config.interface';
+
+async function synchronizeTransfer() {
+  const contractABI = require('../abi/cryptopunk.json');
+  const contractAddress = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB";
+  const provider = new ethers.providers.WebSocketProvider('wss://mainnet.infura.io/ws/v3/' + process.env.INFURA_KEY);
+  const contract = new ethers.Contract(contractAddress, contractABI, provider);
+  contract.on("Transfer", (from, to, value, event) => {
+    let info = {
+      from: from,
+      to: to,
+      value: ethers.utils.formatUnits(value, 6),
+      data: event,
+    };
+
+    var stream = fs.createWriteStream("my_file.txt");
+    stream.once('open', function (fd) {
+      stream.write(JSON.stringify(info, null, 4));
+      stream.end();
+    });
+  });
+}
+synchronizeTransfer();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
