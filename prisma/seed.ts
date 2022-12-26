3c4a7870c7931284fdbe2a59bf6a7a37fcab35bf
snape1112 <107560795+snape1112@users.noreply.github.com>
2022-12-26 20:42:04 +0200

    add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,8 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.user.deleteMany();
   await prisma.post.deleteMany();
+  await prisma.wallet.deleteMany();
+  await prisma.cryptoTransfer.deleteMany();
 
   console.log('Seeding...');
 
@@ -22,6 +24,12 @@ async function main() {
           published: true,
         },
       },
+      wallets: {
+        create: {
+          address: '0x897aEA3D51DCBA918C41aE23F5d9A7411671DeE0',
+          balance: 9
+        }
+      }
     },
   });
   const user2 = await prisma.user.create({
@@ -45,10 +53,32 @@ async function main() {
           },
         ],
       },
+      wallets: {
+        create: {
+          address: '0xa25803ab86A327786Bb59395fC0164D826B98298',
+          balance: 215
+        }
+      }
     },
   });
 
-  console.log({ user1, user2 });
+  const transfer = await prisma.cryptoTransfer.createMany({
+    data: [{
+      from: '0xf5c1d55e94726962b4b517c949120c42d646e455',
+      to: '0x9b338fddc58a3a28042ff3fc9ae3ecbcfe0de267',
+      value: 1,
+      blockNumber: 16270243,
+      blockHash: '0xa5765c30808ef8e9c1ef76469e446d2ebd7da5a01bbd8e72c33ff10b7d4c1588',
+      transactionHash: '0x2ef22f5d3e795914c4baa9333a48f3a48b1d55c41d3ab5d53aa058de53661cac',
+    }, {
+      from: '0x1919db36ca2fa2e15f9000fd9cdc2edcf863e685',
+      to: '0x0232d1083e970f0c78f56202b9a666b526fa379f',
+      value: 1,
+      blockNumber: 16269417,
+      blockHash: '0xde4b6971ad1c42195246457e032d0349295383aee433a0231a029865d43910de',
+      transactionHash: '0xd8f88808570828e2001f459c88edc932fc2eddb31c3e036c6f4aca32c1446593',
+    }]
+  })
 }
 
 main()
